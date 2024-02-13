50da5a01cc8e44aa63c9d4e898330b53468f1afc
amarullz <amarullz@gmail.com>
2024-02-13 19:32:33 +0700

    Tweak ui & responsiveness - Cache animeApi - Set 1.wave as default source - Tweak fullscreen flags

--- app/src/main/java/com/amarullz/androidtv/animetvjmto/AnimeView.java
+++ app/src/main/java/com/amarullz/androidtv/animetvjmto/AnimeView.java
@@ -76,6 +76,7 @@ public class AnimeView extends WebViewClient {
               | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
               | View.SYSTEM_UI_FLAG_IMMERSIVE
               | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+              | View.SYSTEM_UI_FLAG_LOW_PROFILE
               | View.SYSTEM_UI_FLAG_LAYOUT_STABLE;
       decorView.setSystemUiVisibility(uiOptions);
       activity.getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
