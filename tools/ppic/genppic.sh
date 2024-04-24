b238951fae3dec4d359b446a1ff18acbcdcb7788
amarullz <amarullz@gmail.com>
2024-04-25 03:28:11 +0700

    Prevent recommit genppic

--- tools/ppic/genppic.sh
+++ tools/ppic/genppic.sh
@@ -1,3 +1,5 @@
 #!bash
-rm pic/*
-mogrify -format png -path pic -resize 256x256^ -gravity Center -extent 256x256 src/*
+mkdir tmp
+mogrify -format png -path tmp -resize 256x256^ -gravity Center -extent 256x256 src/*
+cp -n tmp/* pic/
+rm -rf tmp
