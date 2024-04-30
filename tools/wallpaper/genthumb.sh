333f630bed6f692e78ff1396f8ac59c83cf8424c
amarullz <amarullz@gmail.com>
2024-04-30 23:03:17 +0700

    Add 9 new submission wallpaper

--- tools/wallpaper/genthumb.sh
+++ tools/wallpaper/genthumb.sh
@@ -1,3 +1,5 @@
 #!bash
-rm thumbs/*
-mogrify -path thumbs -resize 200x200^ -gravity Center -extent 200x200 *
+mkdir tmp
+mogrify -path tmp -resize 200x200^ -gravity Center -extent 200x200 *
+cp -n tmp/* thumbs/
+rm -rf tmp
