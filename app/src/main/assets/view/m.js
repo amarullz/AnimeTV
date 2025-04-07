f5b8c4e10a70034d57fc117181a382f04d326bdd
Ahmad Amarullah <amarullz@gmail.com>
2025-04-07 13:53:57 +0700

    Use kaicodex on m.js

--- app/src/main/assets/view/m.js
+++ app/src/main/assets/view/m.js
@@ -2339,7 +2339,7 @@ var kaas={
 /******************* ANIMEKAI *************************/
 if (__SDKAI){
   // AnimeKai Codex
-  $ap('https://raw.githubusercontent.com/amarullz/AnimeTV/master/tools/utils/kai.js?'+$time(),function(r){
+  $ap('https://raw.githubusercontent.com/amarullz/kaicodex/main/generated/kai_codex.js?'+$time(),function(r){
     if (r.ok){
       try{
         eval(r.responseText+"\n\nwindow.KAICODEX=KAICODEX;");
