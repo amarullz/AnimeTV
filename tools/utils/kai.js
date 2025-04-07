e150b8e4b7497e92a8d45c27e76caf3bf8ab48d0
Ahmad Amarullah <amarullz@gmail.com>
2025-04-07 13:45:37 +0700

    update githubraw url

--- tools/utils/kai.js
+++ tools/utils/kai.js
@@ -1,4 +1,4 @@
-$ap('https://raw.githubusercontent.com/amarullz/kaicodex/refs/heads/main/generated/kai_codex.js?'+$time(),function(r){
+$ap('https://raw.githubusercontent.com/amarullz/kaicodex/main/generated/kai_codex.js?'+$time(),function(r){
   if (r.ok){
     try{
       eval(r.responseText+"\n\nwindow.KAICODEX=KAICODEX;");
