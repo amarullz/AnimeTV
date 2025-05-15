4d7ce1995aadda039690e5dc31bb5cfce8b3a72d
Ahmad <amarullz@gmail.com>
2025-05-16 01:59:16 +0700

    update online encryption

--- tools/utils/kai.js
+++ tools/utils/kai.js
@@ -1,7 +1,76 @@
-$ap('https://raw.githubusercontent.com/amarullz/kaicodex/main/generated/kai_codex.js?'+$time(),function(r){
+$ap('https://raw.githubusercontent.com/amarullz/kaicodex/refs/heads/main/generated/gen/keys.json?'+$time(),function(r){
   if (r.ok){
-    try{
-      eval(r.responseText+"\n\nwindow.KAICODEX=KAICODEX;");
-    }catch(e){}
+    var keys=JSON.parse(r.responseText);
+    var homeKeys=[];
+    var megaKeys=[];
+    for (var i=0;i<keys.kai.length;i++){
+        homeKeys[i]=[];
+        for (var j=0;j<keys.kai[i].length;j++){
+            homeKeys[i].push(atob(keys.kai[i][j]));
+        }
+    }
+    for (var i=0;i<keys.mega.length;i++){
+        megaKeys[i]=[];
+        for (var j=0;j<keys.mega[i].length;j++){
+            megaKeys[i].push(atob(keys.mega[i][j]));
+        }
+    }
+    function encrypt$(n){
+      n = encodeURIComponent(n);
+      var kl=homeKeys[0].length;
+      for (var j=0;j<kl;j++){
+        var o = [];
+        var l = n.length;
+        for (var i = 0; i < l; i++) {
+            o.push(homeKeys[n.charCodeAt(i)][j].charAt(i));
+        }
+        n=btoa(o.join(''));
+      }
+      return n.replace(/\//g, '_').replace(/\+/g, '-').replace(/\=/g, '');
+    }
+    function decrypt$(n){
+      n = n.replace(/_/g, '/').replace(/-/g, '+');
+      var kl=homeKeys[0].length;
+      for (var j=0;j<kl;j++){
+        n=atob(n);
+        var l = n.length;
+        var o = [];
+        for (var i = 0; i < l; i++) {
+            var c=n.charAt(i);
+            for (var k=0;k<homeKeys.length;k++){
+                var ck=homeKeys[k][kl-(j+1)].charAt(i);
+                if (ck===c){
+                    c=String.fromCharCode(k);
+                    break;
+                }
+            }
+            o.push(c);
+        }
+        n=o.join('')
+      }
+      return decodeURIComponent(n);
+    }
+    function megaDec(n){
+      n=n.replace(/_/g, '/').replace(/-/g, '+');
+      var kl=megaKeys[0].length;
+      for (var j=1;j<kl;j++){
+        console.log("ATOB = "+j+" -> "+n);
+        n=atob(n);
+        var o = [];
+        var l = n.length;
+        for (var i = 0; i < l; i++) {
+            var np=n.charCodeAt(i);
+            var ckey=megaKeys[np][j];
+            o.push(ckey.charCodeAt(i%ckey.length));
+        }
+        n=String.fromCharCode.apply(null,o);
+      }
+      return decodeURIComponent(n);
+    }
+    window.KAICODEX={
+      enc:encrypt$,
+      dec:decrypt$,
+      decMega:megaDec
+    };
   }
 });
